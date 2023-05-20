30857b0ec500851dc707123ac0b312611235c68b
Praful Deepak Desai <the.prafuldesai@gmail.com>
2023-05-20 17:35:42 +0530

    Input and Output.cpp

--- Introduction to Cpp/Input and Output.cpp
+++ Introduction to Cpp/Input and Output.cpp
@@ -1,1 +1,28 @@
+/*Objective
+In this challenge, we practice reading input from stdin and printing output to stdout.
+  
+Task
+Read  numbers from stdin and print their sum to stdout.
 
+Input Format
+
+One line that contains  space-separated integers:a, b, and c.*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+  
+    int a,b,c;  
+    cin>>a>>b>>c;
+    
+    int sum=a+b+c;
+    cout<<sum;
+    
+    return 0;
+}
